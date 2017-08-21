71e2861e5ef2ead70e3e60a1315e4eb6b5f7dace
dodolseal <31063014+dodolseal@users.noreply.github.com>
2017-08-21 17:56:33 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -1,145 +1,111 @@
-var Base64 = {
-    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    encode: function (input) {
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-        input = Base64._utf8_encode(input);
-        while (i < input.length) { chr1 = input.charCodeAt(i++); chr2 = input.charCodeAt(i++); chr3 = input.charCodeAt(i++); enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-                enc4 = 64;
-            }
-            output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-        }
-        return output;
-    },
-    decode: function (input) {
-        var output = "";
-        var chr1, chr2, chr3;
-        var enc1, enc2, enc3, enc4;
-        var i = 0;
-        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-        while (i < input.length) {
-            enc1 = this._keyStr.indexOf(input.charAt(i++));
-            enc2 = this._keyStr.indexOf(input.charAt(i++));
-            enc3 = this._keyStr.indexOf(input.charAt(i++));
-            enc4 = this._keyStr.indexOf(input.charAt(i++));
-            chr1 = (enc1 << 2) | (enc2 >> 4);
-            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-            chr3 = ((enc3 & 3) << 6) | enc4;
-            output = output + String.fromCharCode(chr1);
-            if (enc3 != 64) {
-                output = output + String.fromCharCode(chr2);
-            }
-            if (enc4 != 64) {
-                output = output + String.fromCharCode(chr3);
-            }
-        }
-        output = Base64._utf8_decode(output);
-        return output;
-    },
-    _utf8_encode: function (string) {
-        string = string.replace(/\r\n/g, "\n");
-        var utftext = "";
-        for (var n = 0; n < string.length; n++) {
-            var c = string.charCodeAt(n);
-            if (c < 128) { utftext += String.fromCharCode(c); } else if ((c > 127) && (c < 2048)) { utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            } else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-        }
-        return utftext;
-    },
-    _utf8_decode: function (utftext) {
-        var string = "";
-        var i = 0;
-        var c = c1 = c2 = 0;
-        while (i < utftext.length) {
-            c = utftext.charCodeAt(i);
-            if (c < 128) { string += String.fromCharCode(c); i++; } else if ((c > 191) && (c < 224)) {
-                c2 = utftext.charCodeAt(i + 1);
-                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                i += 2;
-            } else {
-                c2 = utftext.charCodeAt(i + 1);
-                c3 = utftext.charCodeAt(i + 2);
-                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                i += 3;
-            }
-        }
-        return string;
-    }
+/*! jquery-base64 | https://github.com/carlo/jquery-base64 */
+"use strict";jQuery.base64=(function($){var _PADCHAR="=",_ALPHA="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",_VERSION="1.0";function _getbyte64(s,i){var idx=_ALPHA.indexOf(s.charAt(i));if(idx===-1){throw"Cannot decode base64"}return idx}function _decode(s){var pads=0,i,b10,imax=s.length,x=[];s=String(s);if(imax===0){return s}if(imax%4!==0){throw"Cannot decode base64"}if(s.charAt(imax-1)===_PADCHAR){pads=1;if(s.charAt(imax-2)===_PADCHAR){pads=2}imax-=4}for(i=0;i<imax;i+=4){b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12)|(_getbyte64(s,i+2)<<6)|_getbyte64(s,i+3);x.push(String.fromCharCode(b10>>16,(b10>>8)&255,b10&255))}switch(pads){case 1:b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12)|(_getbyte64(s,i+2)<<6);x.push(String.fromCharCode(b10>>16,(b10>>8)&255));break;case 2:b10=(_getbyte64(s,i)<<18)|(_getbyte64(s,i+1)<<12);x.push(String.fromCharCode(b10>>16));break}return x.join("")}function _getbyte(s,i){var x=s.charCodeAt(i);if(x>255){throw"INVALID_CHARACTER_ERR: DOM Exception 5"}return x}function _encode(s){if(arguments.length!==1){throw"SyntaxError: exactly one argument required"}s=String(s);var i,b10,x=[],imax=s.length-s.length%3;if(s.length===0){return s}for(i=0;i<imax;i+=3){b10=(_getbyte(s,i)<<16)|(_getbyte(s,i+1)<<8)|_getbyte(s,i+2);x.push(_ALPHA.charAt(b10>>18));x.push(_ALPHA.charAt((b10>>12)&63));x.push(_ALPHA.charAt((b10>>6)&63));x.push(_ALPHA.charAt(b10&63))}switch(s.length-imax){case 1:b10=_getbyte(s,i)<<16;x.push(_ALPHA.charAt(b10>>18)+_ALPHA.charAt((b10>>12)&63)+_PADCHAR+_PADCHAR);break;case 2:b10=(_getbyte(s,i)<<16)|(_getbyte(s,i+1)<<8);x.push(_ALPHA.charAt(b10>>18)+_ALPHA.charAt((b10>>12)&63)+_ALPHA.charAt((b10>>6)&63)+_PADCHAR);break}return x.join("")}return{decode:_decode,encode:_encode,VERSION:_VERSION}}(jQuery));
+var Safelink = new Safelink();
+function Safelink(){
+	this.active = false;
+	this.url = [];
+	this.api = function(json){
+		var dasar  = json.feed.entry;
+		var jumlah = dasar.length;
+		this.url["url"] = json.feed.link[2].href;
+		for(var indeksEntri = 0; indeksEntri < jumlah; indeksEntri++){
+			if(indeksEntri == jumlah){
+				break;
+			}
+			var tautan = dasar[indeksEntri].link[4].href;
+			this.url.push(tautan);
+		}
+	}
+	this.get = function(){
+		var filter = location.search.substr(1).split("&");
+		var indeks = [];
+		for(var indeksFilter = 0; indeksFilter < filter.length; indeksFilter++){
+			var hasil = filter[indeksFilter].split("=");
+			indeks[hasil[0]] = decodeURIComponent(hasil[1]);
+		}
+		return indeks;
+	}
+	this.cek = function(data){
+		if(typeof data != "undefined"){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	this.run = function(){
+		var $_GET = Safelink.get();
+		if(Safelink.cek($_GET["data"])){
+			var acak = Math.floor((Math.random() * Safelink.url.length) + 0);
+			location.href = Safelink.url[acak] + "?url=" + $_GET["data"];
+		}else if(Safelink.url.indexOf(location.href.replace(location.search, "").replace(location.origin + "/", Safelink.url["url"])) != -1 && Safelink.cek($_GET["url"]) == true){
+			$(".post-body").html("<div class=\"fyline-generate\"></div>" + $(".post-body").html() + "<div class=\"fyline-visit\"></div>");
+			$(".fyline-generate")
+			.css("display", "block")
+			.css("text-align", "center")
+			.css("background-color", "green")
+			.css("color", "white")
+			.css("padding", "5px 10px")
+			.css("font-size", "14px")
+			.css("border", "1px solid rgba(0, 0, 0, 0.3)")
+			.css("cursor", "pointer")
+			.css("border-radius", "5px")
+			.css("font-family", "monospace")
+			.css("margin-bottom", "10px")
+			.attr("onclick", "Safelink.generate()")
+			.html("Click Here to Generate");
+		}
+	}
+	this.generate = function(){
+		Safelink.active = true;
+		var seconds   = $("script.fyline-safelink").attr("data-timer");
+		var countdown = setInterval(function(){
+			seconds--;
+			if(seconds > 0){
+				$(".fyline-generate").css("cursor", "not-allowed");
+				$(".fyline-generate").css("opacity", "0.8");
+				$(".fyline-generate").html("Wait in " + seconds + " seconds.");
+			}else if(seconds == 0){
+				clearInterval(countdown);
+				$(".fyline-generate").css("opacity", "1");
+				$(".fyline-generate").html("Click here!").css("cursor", "pointer").attr("onclick", "Safelink.scroll()");
+			}
+		}, 1000);
+	}
+	this.scroll = function(){
+		$(".fyline-generate").fadeOut("slow");
+		$("html, body").animate({
+			scrollTop: jQuery(".fyline-visit").offset().top
+		}, 2000);
+		Safelink.visit();
+	}
+	this.visit = function(){
+		var seconds = 4;
+		$(".fyline-visit")
+		.css("display", "block")
+		.css("text-align", "center")
+		.css("background-color", "green")
+		.css("color", "white")
+		.css("padding", "5px 10px")
+		.css("font-size", "14px")
+		.css("border", "1px solid rgba(0, 0, 0, 0.3)")
+		.css("cursor", "pointer")
+		.css("border-radius", "5px")
+		.css("font-family", "monospace")
+		.css("margin", "10px 0")
+		.css("opacity", "0.8")
+		.html("Please wait...");
+		var countdown = setInterval(function(){
+			seconds--;
+			if(seconds == 0){
+				clearInterval(countdown);
+				$(".fyline-visit").attr("onclick", "Safelink.out()").html("Visit URL").css("opacity", "1");
+			}
+		}, 1000);
+	}
+	this.out = function(){
+		location.href = $.base64.decode(Safelink.get()["url"]);
+	}
 }
-var encode = document.getElementById('encode'),
-    decode = document.getElementById('decode'),
-    output = document.getElementById('output'),
-    input = document.getElementById('input');
-var User_ID = "";
-var protected_links = "";
-var a_to_va = 0;
-var a_to_vb = 0;
-var a_to_vc = "";
-
-function auto_safelink() {
-    auto_safeconvert();
-}
-
-function auto_safeconvert() {
-    var a_to_vd = window.location.hostname;
-    if (protected_links != "" && !protected_links.match(a_to_vd)) {
-        protected_links += ", " + a_to_vd;
-    } else if (protected_links == "") {
-        protected_links = a_to_vd;
-    }
-    var a_to_ve = "";
-    var a_to_vf = new Array();
-    var a_to_vg = 0;
-    a_to_ve = document.getElementsByTagName("a");
-    a_to_va = a_to_ve.length;
-    a_to_vf = a_to_fa();
-    a_to_vg = a_to_vf.length;
-    var a_to_vh = false;
-    var j = 0;
-    var a_to_vi = "";
-    for (var i = 0; i < a_to_va; i++) {
-        a_to_vh = false;
-        j = 0;
-        while (a_to_vh == false && j < a_to_vg) {
-            a_to_vi = a_to_ve[i].href;
-            if (a_to_vi.match(a_to_vf[j]) || !a_to_vi || !a_to_vi.match("http")) {
-                a_to_vh = true;
-            }
-            j++;
-        }
-        if (a_to_vh == false) {
-            var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://safe-linknya.blogspot.com/p/goto.html?url=" + encryptedUrl;
-            a_to_ve[i].rel = "nofollow";
-            a_to_vb++;
-            a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
-        }
-    }
-    var a_to_vj = document.getElementById("anonyminized");
-    var a_to_vk = document.getElementById("found_links");
-    if (a_to_vj) {
-        a_to_vj.innerHTML += a_to_vb;
-    }
-    if (a_to_vk) {
-        a_to_vk.innerHTML += a_to_va;
-    }
-}
-
-function a_to_fa() {
-    var a_to_vf = new Array();
-    protected_links = protected_links.replace(" ", "");
-    a_to_vf = protected_links.split(",");
-    return a_to_vf;
-}
+$(document).ready(function(){
+	Safelink.run();
+});
